9dc452053f320c3525f5165384588e160f51b516
Caleb-Beard <caleb.beard@my.uwi.edu>
2018-09-17 16:09:49 -0400

    17/09/18

--- lab1.c
+++ lab1.c
@@ -1,23 +1,29 @@
-/* Lab 1 first file - ID number 815009291 Group H  */
+
+/* Lab 1 first file - ID number 814005504 Group A */
 #include <p18f452.h>   
 #include <delays.h>
  /* Set configuration bits for use with PICKit3 and 4MHz oscillator */
 #pragma config OSC = HS
 #pragma config WDT = OFF
 #pragma config LVP = OFF
-#define XTAL_FREQ 4000000
 
-
+int counter;
 
  void main (void)
 {
-     //test
-    /* Insert code from pre-lab Q14 to change values on PORTB */
+     TRISAbits.TRISA3 = 0;
+     
+     LATAbits.LATA3 = 0;
+     
      TRISB = 0;
-     for (int i =1, i<16,i++)
+     
+    /* Insert code from pre-lab Q14 to change values on PORTB */
+     for( counter = 1; counter <=15; counter++ )
      {
-         PORTB = i;
-         void Delay10KTCYx (0x32);
+         PORTB = counter;
+         
+         Delay10KTCYx(200);
+         
          
      }
 }
