10aa7d1dcc7f7ae5d9d13a98a4cf72b68d23e673
Caleb-Beard <caleb.beard@my.uwi.edu>
2018-09-17 14:50:04 -0400

    17/09/18

---
+++ Blink.c
@@ -0,0 +1,25 @@
+#include <p18cxxx.h>
+#include <delays.h>
+
+//Set configuration Bits
+#pragma config WDT = OFF
+#pragma config OSC = HS
+
+void main(void)
+{
+
+      TRISDbits.TRISD7 = 0; // Set Pin RD7 as an output
+  
+  while(1)
+  {  
+        LATDbits.LATD7 = 1;// Set RD7 High
+   
+        Delay10KTCYx(200); //Wait for some time (two seconds)
+    
+         LATDbits.LATD7= 0;// Set RD7 Low
+    
+         Delay10KTCYx(200); //Wait for some time (two seconds) 
+   
+ 
+  }
+}
