20ae0133116d7e1414da0cc2fed6e3a5638ad85e
seenathgooding <seenathgooding@gmail.com>
2018-09-25 19:02:28 -0400

    Update lab1.c
    fixing git

--- lab1.c
+++ lab1.c
@@ -1,12 +1,25 @@
-/* Lab 1 first file - ID number xxxxxx Group xxx  */
-#include <p18f452.h>   
-
-/* Set configuration bits for use with PICKit3 and 4MHz oscillator */
+/* Lab 1 first file - ID number 814117267 Group H  */
+#include <p18f452.h>
+#include <delays.h>
+ /* Set configuration bits for use with PICKit3 and 4MHz oscillator */
 #pragma config OSC = HS
 #pragma config WDT = OFF
 #pragma config LVP = OFF
+#define XTAL_FREQ 4000000
+int x =1;
 
-void main (void)
+ void main (void)
 {
     /* Insert code from pre-lab Q14 to change values on PORTB */
+     
+     /*output on the pins of portb in bin a variable that counts 1 to 15*/
+     
+     /*initialise portb*/
+     
+     TRISB = 0;
+     for (x; x <=15; x++){
+         PORTB = x;
+        
+         /*testdf*/
+     }
 }
