d3ed75b7b55b411b4ba5f6e909802effa1b7126f
815008434 <815008434@MICRO-P-LAB-02.saelec.uwi.tt>
2019-09-25 16:00:06 -0400

    Initial Commit - 1Hour Elapsed

--- lab1.c
+++ lab1.c
@@ -1,6 +1,7 @@
-/* Lab 1 first file - ID number 816002558 Group d  */
+/* Lab 1 first file - ID number 815008434 Group E  */
 #include <p18f452.h>   
 #include <delays.h>
+
  /* Set configuration bits for use with PICKit3 and 4MHz oscillator */
 #pragma config OSC = HS
 #pragma config WDT = OFF
@@ -11,13 +12,6 @@
 
  void main (void)
 {
-     //test
+    //test
     /* Insert code from pre-lab Q14 to change values on PORTB */
-     TRISB = 0;
-     for (int i =1, i<16,i++)
-     {
-         PORTB = i;
-         void Delay10KTCYx (0x32);
-         
-     }
 }
