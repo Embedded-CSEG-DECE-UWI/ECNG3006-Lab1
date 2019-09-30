fbec8e870bab7f13805782fdefe3a2b0b33257be
Amritta <abigail.ramkissoon@my.uwi.edu>
2019-09-30 04:08:29 -0400

    working ASM commit

--- lab1.asm
+++ lab1.asm
@@ -1,30 +1,30 @@
 			LIST		p=18f452
 			INCLUDE		<P18F452.INC>
 				
-	    		CBLOCK		0x480
-			MapName:	8
-			ENDC
+	    		UDATA		0x480
+MapName			RES		8
+			;ENDC
 
 			EXTERN		MapIndex
 filter			CODE
 
 ;All digits moved into each register are the Hexadecimal conversions of ASCII characters
-MapNamee		movlw		0x3D		;M
-			movwf		0x480
-			movlw		0x3D		;a
-			movwf		0x481		
-			movlw		0x3D		;p
-			movwf		0x482	
-			movlw		0x3D		;N
-			movwf		0x483
-			movlw		0x3D		;a
-			movwf		0x484
-			movlw		0x3D		;m
+MapNamee		movlw		0x54		;T
+			movwf		0x482
+			movlw		0x61		;a
+			movwf		0x483		
+			movlw		0x6B		;k
+			movwf		0x484	
+			movlw		0x65		;e
 			movwf		0x485
-			movlw		0x3D		;e
+			movlw		0x20		;SPACE
 			movwf		0x486
-			movlw		0x00		;NULL
+			movlw		0x32		;2
 			movwf		0x487
+			movlw		0x20		;SPACE
+			movwf		0x488
+			movlw		0x00		;NULL
+			movwf		0x489
 
 getMapChar		movlw		0x07
 			cpfsgt		MapIndex, 0
@@ -34,7 +34,7 @@ getMapChar		movlw		0x07
 getInvalidChar		movlw		0xFF
 			return
 				
-getChar			lfsr		FSR0, 0x480
+getChar			lfsr		FSR0, 0x482
 			movf		MapIndex, W
 			movf		PLUSW0, W
 			return
--- lab1asm.c
+++ lab1asm.c
@@ -1,51 +1,55 @@
-/* Lab 1 first file - ID number 816000924 Group F  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <p18f452.h> 
 #include <delays.h>
-#include "xlcd.h"
+#include "xlcd_A.h"
 
- /* Set configuration bits for use with PICKit3 and 4MHz oscillator */
+
+/* Set configuration bits for use with PICKit3 and 4MHz oscillator */
 #pragma config OSC = XT
 #pragma config WDT = OFF
 #pragma config LVP = OFF
 
-unsigned int MapIndex = 0;
+unsigned int MapIndex;
 extern char getMapChar(void);
 extern void MapNamee(void);
 extern char MapName;
-char groupName[7];
+char groupName[8];
 
-int cool;
 
+//PreLab Q16a
 void DelayFor18TCY(void)
- {
-         Nop(); Nop(); Nop(); Nop(); Nop(); Nop();
-         Nop(); Nop(); Nop(); Nop(); Nop(); Nop();
-         Nop(); Nop(); Nop(); Nop(); Nop(); Nop();         
- }
+{
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+    Nop();
+}
  
-void DelayXLCD(void){                  //1000us = 1ms
-     Delay1KTCYx(5);
-     
- }
- 
-void DelayPORXLCD(void){
-     Delay1KTCYx(15);
- }
- 
-/*void init_lcd(void)
-{ 
-     OpenXLCD(FOUR_BIT & LINES_5X7);
-     while(BusyXLCD());
-     //WriteCmdXLCD(FOUR_BIT & LINES_5X7);
-     //while(BusyXLCD());
-     //WriteCmdXLCD( BLINK_ON );
-     //while(BusyXLCD());
-     WriteCmdXLCD(SHIFT_DISP_LEFT);
-}*/
+//PreLab Q16b
+void DelayXLCD (void)
+{
+    Delay1KTCYx(5);    
+    return;
+}
 
+//PreLab Q16c
+void DelayPORXLCD (void)
+{
+    Delay1KTCYx(15);    
+    return;
+}
+ 
 void setup (void)
 {
     DelayPORXLCD();
@@ -59,17 +63,15 @@ void setup (void)
 
 void main (void)
 {
+    Nop();
     MapNamee();    
-    for (MapIndex = 0; MapIndex <= 6; MapIndex++){
-        groupName[MapIndex] = getMapChar();
-    } 
     setup();
     DelayPORXLCD(); 
     while(BusyXLCD());
-    SetDDRamAddr(0x40);
-    while(BusyXLCD());
-    putsXLCD(groupName);
+    //SetDDRamAddr(0x40);
     //while(BusyXLCD());
-    while (1)
-    {}
+    putsXLCD(groupName);
+    DelayPORXLCD();
+    while (1) {}
 }
+
