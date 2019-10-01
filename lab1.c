ebbdc144a8090e0c1693d324bb14b018c53d7355
Kershawn Franklyn <kershawn@hotmail.com>
2019-10-01 15:50:55 -0400

    Question 1 commit

--- lab1.c
+++ lab1.c
@@ -1,24 +1,24 @@
-/* Lab 1 first file - ID number 816002558 Group d  */
+/* Lab 1 first file - ID number 81601481 Group G  */
 #include <p18f452.h>   
-#include <delays.h>
+
  /* Set configuration bits for use with PICKit3 and 4MHz oscillator */
 #pragma config OSC = HS
 #pragma config WDT = OFF
 #pragma config LVP = OFF
-#define XTAL_FREQ 4000000
-
 
 
  void main (void)
 {
-     //test
-    /* Insert code from pre-lab Q14 to change values on PORTB */
-     TRISB = 0;
-     for (int i =1, i<16,i++)
-     {
-         PORTB = i;
-         void Delay10KTCYx (0x32);
-         
-     }
+    int count = 0b0001;
+	TRISB = 0;
+	while (count <= 0b1111)
+	{
+		PORTB = count;
+		count++; 	
+        //Delay1KTCYx(3500);
+	}
+    
+   // Sleep();
 }
  
+ /*Simplier comment that can be commited*/
