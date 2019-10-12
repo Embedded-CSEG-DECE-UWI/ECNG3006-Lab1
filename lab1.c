f444c30da4e0513be0cb6116541e83a076927bf2
Caleb-Beard <caleb.beard@my.uwi.edu>
2019-10-12 00:09:58 -0400

    Merge origin/master

--- lab1.c
+++ lab1.c
@@ -1,24 +1,26 @@
-/* Lab 1 first file - ID number 816002558 Group d  */
-#include <p18f452.h>   
+/* Lab 1 first file - ID number 814005504 Group F  */
+#include <p18cxxx.h>   
 #include <delays.h>
- /* Set configuration bits for use with PICKit3 and 4MHz oscillator */
+#include <portb.h>
+
+/* Set configuration bits for use with PICKit3 and 4MHz oscillator */
 #pragma config OSC = HS
 #pragma config WDT = OFF
 #pragma config LVP = OFF
+
 #define XTAL_FREQ 4000000
 
-
-
- void main (void)
+int count;
+void main (void)
 {
-     //test
     /* Insert code from pre-lab Q14 to change values on PORTB */
-     TRISB = 0;
-     for (int i =1, i<16,i++)
-     {
-         PORTB = i;
-         void Delay10KTCYx (0x32);
-         
-     }
+    //count = 1;
+    TRISB = 0;      //config PORTB as output
+    for(count = 1; count<=15; count++)
+    {
+        PORTB = count;  //displays count
+        //Nop();
+        Delay10KTCYx (200);
+    }
+    
 }
- 
